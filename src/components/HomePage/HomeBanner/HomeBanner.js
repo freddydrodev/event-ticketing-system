04bf2179b5868ad02f2973cd3efc2a55d6b07160
Fredius Tout Court <frediustcdev@gmail.com>
2018-08-21 21:22:29 +0000

    banner added but not styled well
    also need to remove some border-0 classes

--- src/components/HomePage/HomeBanner/HomeBanner.js
+++ src/components/HomePage/HomeBanner/HomeBanner.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import "./HomeBanner.less";
 
+import img1 from "../../../assets/images/1.jpeg";
+import img2 from "../../../assets/images/2.jpeg";
+import img3 from "../../../assets/images/3.jpeg";
+import img4 from "../../../assets/images/4.jpeg";
+
 const settings = {
   dots: true,
   infinite: true,
@@ -14,7 +19,18 @@ export default class HomeBanner extends Component {
     return (
       <div>
         <Slider {...settings} className={`bg-white`}>
-          <div style={{ minHeight: 60 }}>Slider</div>
+          <div style={{ minHeight: 60, height: 350, overflow: "hidden" }}>
+            <img src={img1} alt={`cover`} className={`w-100`} />
+          </div>
+          <div style={{ minHeight: 60, height: 350, overflow: "hidden" }}>
+            <img src={img2} alt={`cover`} className={`w-100`} />
+          </div>
+          <div style={{ minHeight: 60, height: 350, overflow: "hidden" }}>
+            <img src={img3} alt={`cover`} className={`w-100`} />
+          </div>
+          <div style={{ minHeight: 60, height: 350, overflow: "hidden" }}>
+            <img src={img4} alt={`cover`} className={`w-100`} />
+          </div>
         </Slider>
       </div>
     );
